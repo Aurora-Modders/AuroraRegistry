314562e5a58acf4f36a6228a94505d4680e718f5
01010100b <caveman1917@hotmail.com>
2020-04-25 08:48:39 +0200

    add registry files and aurora checksum

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,21 @@ namespace AuroraRegistry
     {
         static void Main(string[] args)
         {
+            var exe = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Aurora.exe");
+            var checksum = GetChecksum(File.ReadAllBytes(exe));
+            Console.WriteLine(checksum);
+            Console.ReadLine();
+        }
 
+        public static string GetChecksum(byte[] bytes)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var hash = sha.ComputeHash(bytes);
+                var str = Convert.ToBase64String(hash);
+
+                return str.Replace("/", "").Replace("+", "").Replace("=", "").Substring(0, 6);
+            }
         }
     }
 }
