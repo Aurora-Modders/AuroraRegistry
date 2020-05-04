324bb1966fde9a07df98858b383ad84bfce92bc2
Matt Alioto <malioto@wayfair.com>
2020-05-03 22:29:58 -0400

    Quick brush-up of the powershell

--- Checksum/Get-Checksum.ps1
+++ Checksum/Get-Checksum.ps1
@@ -1,4 +1,10 @@
 $auroraExe = $args[0]
+if (-Not (Test-Path $auroraExe))
+{
+    Write-Warning "Couldn't find $auroraExe. Call this script with the path to aurora.exe."
+    return
+}
+
 $bytes = [System.IO.File]::ReadAllBytes($auroraExe)
 $hash = [System.Security.Cryptography.SHA256]::Create().ComputeHash($bytes)
 $base64 = [Convert]::ToBase64String($hash)
